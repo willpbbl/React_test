5e3a39e7e2b3cf3adb10825608af6dfaa1e03b1b
Will Betts <will@Wills-MacBook-Pro-2.local>
2017-09-25 13:28:08 -0400

    added port to production webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -25,7 +25,8 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+        'NODE_ENV': JSON.stringify('production'),
+        'API_HOST': 'https://react-test-pp.herokuapp.com/'
       }
     }),
   ],
